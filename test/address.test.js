31b04f35cb982e9e1a1238d710268318d4a9dfcd
JP Richardson <jprichardson@gmail.com>
2013-11-04 23:27:23 -0600

    implementation complete

--- test/address.test.js
+++ test/address.test.js
@@ -1,6 +1,17 @@
-
+var Address = require('../lib/address')
+  , conv = require('cryptocoin-convert-hex')
 
 require('terst')
+
+describe('Address', function() {
+  describe('> when a the result of the sha256 ripemd160 is input', function() {
+    it('should create the bitcoin address', function() {
+      var hash160 = "3c176e659bea0f29a3e9bf7880c112b1b31b4dc8"
+      var address = new Address(conv.hexToBytes(hash160), 'prod') //'testnet is also valid'
+      EQ (address.toString(), "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS")
+    })
+  })
+})
 
 
 
